29bd87460df57993f27dea254ca675c82eea400b
root <1046762075@qq.com>
2020-04-15 21:59:35 +0800

    Seata

---
+++ Seata/seata_order.sql
@@ -0,0 +1,60 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Nay
+ Source Server Type    : MySQL
+ Source Server Version : 50729
+ Source Host           : localhost:3306
+ Source Schema         : seata_order
+
+ Target Server Type    : MySQL
+ Target Server Version : 50729
+ File Encoding         : 65001
+
+ Date: 15/04/2020 21:57:35
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(11) NULL DEFAULT NULL COMMENT '用户id',
+  `product_id` bigint(11) NULL DEFAULT NULL COMMENT '产品id',
+  `count` int(11) NULL DEFAULT NULL COMMENT '数量',
+  `money` decimal(11, 0) NULL DEFAULT NULL COMMENT '金额',
+  `status` int(1) NULL DEFAULT NULL COMMENT '订单状态：0：创建中;1：已完结',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_order
+-- ----------------------------
+INSERT INTO `t_order` VALUES (1, 1, 1, 10, 100, 1);
+INSERT INTO `t_order` VALUES (2, 1, 1, 10, 110, 1);
+INSERT INTO `t_order` VALUES (3, 1, 1, 10, 120, 1);
+INSERT INTO `t_order` VALUES (4, 1, 1, 10, 130, 0);
+
+-- ----------------------------
+-- Table structure for undo_log
+-- ----------------------------
+DROP TABLE IF EXISTS `undo_log`;
+CREATE TABLE `undo_log`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `context` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime(0) NOT NULL,
+  `log_modified` datetime(0) NOT NULL,
+  `ext` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `ux_undo_log`(`xid`, `branch_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
